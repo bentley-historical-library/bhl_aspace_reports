90309c6d0a201e3cbeed175960c9b5288332a729
djpillen@umich.edu <djpillen@umich.edu>
2017-07-05 14:10:16 -0400

    fix some bugs in dart report

--- backend/model/bhl_dart_report.rb
+++ backend/model/bhl_dart_report.rb
@@ -23,7 +23,10 @@ class BhlDARTReport < AbstractReport
     end
 
     @from = DateTime.parse(from).to_time.strftime("%Y-%m-%d %H:%M:%S")
-    @to = DateTime.parse(to).to_time.strftime("%Y-%m-%d %H:%M:%S")
+
+    # add 1 day to the to parameter to make the Sequel query inclusive of the date specified in the to parameter
+    to_parsed = DateTime.parse(to) + 1
+    @to = to_parsed.to_time.strftime("%Y-%m-%d %H:%M:%S")
   end
 
 
@@ -32,7 +35,7 @@ class BhlDARTReport < AbstractReport
   end
 
   def headers
-    ['DART_LID', 'Lastname', 'RestOfName', 'Suffix', 'Title', 'OrganizationOrUnit', 'Street1', 'Street2', 'City', 'St', 'Zip', 'donation amt', 'credit date', 'item description', 'designation', 'comment', 'concatenated', 'note', 'note type', 'GIK subtype', '# of units', 'item name', 'REV type', 'do not receipt', 'Constituent alt lookup ID', 'Bentley Accession ID']
+    ['DART_LID', 'Lastname', 'Firstname', 'Middle', 'Suffix', 'Title', 'OrganizationOrUnit', 'Street1', 'Street2', 'City', 'St', 'Zip', 'donation amt', 'credit date', 'item description', 'designation', 'comment', 'concatenated', 'note', 'note type', 'GIK subtype', '# of units', 'item name', 'REV type', 'do not receipt', 'Constituent alt lookup ID', 'Bentley Accession ID']
   end
 
   def processor
@@ -63,7 +66,7 @@ class BhlDARTReport < AbstractReport
       Sequel.qualify(:accession, :identifier),
       Sequel.qualify(:accession, :accession_date).as(:accession_date),
       Sequel.as(Sequel.lit('GetAgentLastName(linked_agents_rlshp.agent_person_id, linked_agents_rlshp.agent_family_id, linked_agents_rlshp.agent_corporate_entity_id)'), :Lastname),
-      Sequel.as(Sequel.lit('GetAgentRestOfName(linked_agents_rlshp.agent_person_id, linked_agents_rlshp.agent_family_id, linked_agents_rlshp.agent_corporate_entity_id)'), :RestOfName),
+      Sequel.as(Sequel.lit('GetAgentRestOfName(linked_agents_rlshp.agent_person_id, linked_agents_rlshp.agent_family_id, linked_agents_rlshp.agent_corporate_entity_id)'), :Firstname),
       Sequel.as(Sequel.lit('GetAgentSuffix(linked_agents_rlshp.agent_person_id, linked_agents_rlshp.agent_family_id, linked_agents_rlshp.agent_corporate_entity_id)'), :Suffix),
       Sequel.as(Sequel.lit('GetAgentTitle(linked_agents_rlshp.agent_person_id, linked_agents_rlshp.agent_family_id, linked_agents_rlshp.agent_corporate_entity_id)'), :Title),
       Sequel.as(Sequel.lit('GetAgentOrganizationOrUnit(linked_agents_rlshp.agent_person_id, linked_agents_rlshp.agent_family_id, linked_agents_rlshp.agent_corporate_entity_id)'), :OrganizationOrUnit),
@@ -75,7 +78,9 @@ class BhlDARTReport < AbstractReport
       Sequel.as(Sequel.lit('GetAgentDonorNumber(linked_agents_rlshp.agent_person_id, linked_agents_rlshp.agent_family_id, linked_agents_rlshp.agent_corporate_entity_id)'), :donor_number),
       Sequel.as(Sequel.lit('GetAgentDARTLID(linked_agents_rlshp.agent_person_id, linked_agents_rlshp.agent_family_id, linked_agents_rlshp.agent_corporate_entity_id)'), :DART_LID)
       ).
-    where(Sequel.qualify(:accession, :repo_id) => @repo_id)
+    where(Sequel.qualify(:accession, :repo_id) => @repo_id).
+    group(Sequel.qualify(:accession, :id))
+
 
     dataset = dataset.where(Sequel.lit('GetEnumValue(user_defined.enum_1_id)') => ["MHC", "FAC"]).or(Sequel.lit('GetEnumValue(user_defined.enum_2_id)') => ["MHC", "FAC"]).or(Sequel.lit('GetEnumValue(user_defined.enum_3_id)') => ["MHC", "FAC"])
 
