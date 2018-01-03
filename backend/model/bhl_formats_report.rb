234995eb104318330da095603f8edaf17d26b816
djpillen@umich.edu <djpillen@umich.edu>
2018-01-03 11:27:21 -0500

    refactor bhl_formats_report

--- backend/model/bhl_formats_report.rb
+++ backend/model/bhl_formats_report.rb
@@ -4,54 +4,38 @@ class BhlFormatsReport < AbstractReport
                     :params => [["Formats", "formatsparams", "The formats of interest"]]
                   })
 
+  
+  def permutate_format(format)
+    permutations = []
+    singular_format = format.gsub(/s$/, "")
+    plural_format = format.gsub(/(?<!s)$/, "s")
+    [singular_format, plural_format].each do |format_variation|
+      permutations << format_variation
+      # Replace spaces with underscores
+      permutations << format_variation.gsub(" ", "_")
+      # Remove spaces
+      permutations << format_variation.gsub(" ", "")
+      # Remove dashes
+      permutations << format_variation.gsub("-", "")
+      # Replace dashes with spaces
+      permutations << format_variation.gsub("-", " ")
+    end
+
+    permutations
+  end
+
   def initialize(params, job, db)
     super
 
-    submitted_formats = JSON.parse(@job.job_blob)["additional_params"]
-    submitted_formats = submitted_formats.gsub("\n", ", ")
-    submitted_formats = submitted_formats.split(%r{,\s*})
-    submitted_formats = submitted_formats.map{|format| format.downcase}
-
+    submitted_formats = ASUtils.json_parse(@job.job_blob)["additional_params"].split(",")
+    @formats_array = []
     submitted_formats.each do |format|
-      if format.end_with?("s")
-        singular_format = format.gsub(/s$/, "")
-        if !submitted_formats.include?(singular_format)
-          submitted_formats << singular_format
-        end
-      else
-        plural_format = format + "s"
-        if !submitted_formats.include?(plural_format)
-          submitted_formats << plural_format
-        end
-      end
-
-      if format.include?(" ")
-        underscore_format = format.gsub(" ", "_")
-        if !submitted_formats.include?(underscore_format)
-          submitted_formats << underscore_format
-        end
-
-        no_space_format = format.gsub(" ", "")
-        if !submitted_formats.include?(no_space_format)
-          submitted_formats << no_space_format
-        end
-      end
-
-      if format.include?("-")
-        space_format = format.gsub("-", " ")
-        if !submitted_formats.include?(space_format)
-          submitted_formats << space_format
-        end
-
-        no_dash_format = format.gsub("-", "")
-        if !submitted_formats.include?(no_dash_format)
-          submitted_formats << no_dash_format
-        end
-      end
-
+      @formats_array << format.downcase.strip
+      @formats_array.concat permutate_format(format.downcase.strip)
     end
-    @formats_array = submitted_formats
-    @formats_regex = Regexp.new submitted_formats.join("|")
+
+    @formats_array.uniq!
+    @formats_regex = Regexp.union(@formats_array)
   end
 
 
@@ -60,19 +44,19 @@ class BhlFormatsReport < AbstractReport
   end
 
   def headers
-    ['display_string', 'extents', 'physical_details_note', 'containers', 'breadcrumb', 'call_number', 'resource_id', 'archival_object_id']
+    ['display_string', 'extents', 'containers', 'physical_details_note', 'breadcrumb', 'call_number', 'resource_id', 'archival_object_id']
   end
 
   def processor
     {
       'call_number' => proc {|record| ASUtils.json_parse(record[:resource_identifier] || "[]").compact.join("-")},
-      'physical_details_note' => proc {|record| if record[:notes] && JSON.parse(record[:notes])["type"] == "physfacet"
-                                                  JSON.parse(record[:notes])["content"][0]
+      'physical_details_note' => proc {|record| if record[:notes]
+                                                  ASUtils.json_parse(record[:notes])["content"][0]
                                                 else
                                                   ""
                                                 end
                                       }
-
+  
     }
   end
 
@@ -81,7 +65,7 @@ class BhlFormatsReport < AbstractReport
     dataset
   end
 
-  def query    
+  def query 
     dataset = db[:archival_object].
     left_outer_join(:extent, Sequel.qualify(:extent, :archival_object_id) => Sequel.qualify(:archival_object, :id)).
     left_outer_join(:note, Sequel.qualify(:note, :archival_object_id) => Sequel.qualify(:archival_object, :id)).
@@ -95,7 +79,7 @@ class BhlFormatsReport < AbstractReport
       Sequel.as(Sequel.lit('GetArchivalObjectContainers(archival_object.id)'), :containers),
       Sequel.as(Sequel.lit('GetArchivalObjectBreadcrumb(archival_object.id)'), :breadcrumb)
       ).
-    where(Sequel.lit('GetEnumValue(extent.extent_type_id)') => @formats_array).
+    where(Sequel.lit('GetEnumValue(extent.extent_type_id)') => @formats_array)#.
     or(Sequel.ilike(Sequel.qualify(:extent, :physical_details), @formats_regex)).
     or(Sequel.ilike(Sequel.lit('GetArchivalObjectNoteByType(archival_object.id, "physfacet")'), @formats_regex)).
     group(Sequel.qualify(:archival_object, :id))
