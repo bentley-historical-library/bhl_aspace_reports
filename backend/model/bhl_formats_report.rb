1233ea3ce101255356b58f18a01c739664053fea
Mark Triggs <mark@dishevelled.net>
2023-05-02 19:22:31 +1000

    Make sure the query_string method only runs once per instance of this report
    It's nice to be able to print the query string for debugging purposes
    without making a mess of the internal state of the object.

--- backend/model/bhl_formats_report.rb
+++ backend/model/bhl_formats_report.rb
@@ -206,6 +206,12 @@ class BhlFormatsReport < AbstractReport
   end
 
   def query_string
+    # Cache the result since this method actually has side effects, and that can be
+    # surprising if you're just trying to print out the query string for debugging.
+    @computed_query_string ||= query_string_compute
+  end
+
+  def query_string_compute
     archival_object_ids = []
     archival_object_to_extent_ids = query_extents()
     extent_archival_object_ids = archival_object_to_extent_ids.keys
