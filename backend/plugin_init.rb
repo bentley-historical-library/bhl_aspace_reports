828e51abd0feec8fc7bdffb87a202e556f7cfa3c
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-12-08 10:06:41 +0800

    Delete backend directory

--- backend/plugin_init.rb
+++
@@ -1,4 +0,0 @@
-# We depend on this enum value being here.  Make sure it is.
-RequestContext.open(:create_enums => true) do
-  BackendEnumSource.valid?('linked_agent_event_roles', 'field_archivist')
-end
